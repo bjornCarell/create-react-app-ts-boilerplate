88f08b7f878598f53b453177643b4db194ac8fbd
Bjorn Carell <bjorn.carell@tink.com>
2021-08-21 11:04:56 +0200

    feat(project): init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <h3>Create React App - TS</h3>
+      <p>Boilerplate</p>
+    </div>
+  );
+}
+
+export default App;
